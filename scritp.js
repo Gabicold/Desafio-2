088a9c5e3a9c590680b66c23a0b30f2a03cdad39
Gabicold <biel18102004@gmail.com>
2022-01-19 17:36:04 -0300

    commit 1

---
+++ scritp.js
@@ -0,0 +1,15 @@
+let idade = 18
+let humano = true
+let aniversario = "dezembro"
+if(idade >= 18){
+    console.log("Voce é de maior!! Pode entrar.")
+}else{
+    console.log("Voce nao pode entrar,.Vaza!!")
+}
+if(humano = true){
+    console.log("Parabéns você não é um ET")
+}
+if(aniversario == "janeiro" || aniversario == "dezembro" ){
+    console.log("Você faz aniversário nos meses indicados!")
+}
+
